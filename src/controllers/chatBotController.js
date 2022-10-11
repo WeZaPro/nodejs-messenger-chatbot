b20f365f6e448cfdad9a20b01fde7874e22e3878
WeZaPro <taweesak9359@gmail.com>
2022-10-11 17:56:18 +0700

    first commit40

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -155,6 +155,7 @@ function handleMessage(sender_psid, received_message) {
         callSendAPI2(sender_psid, response);
         callSendAPI(sender_psid, received_message.text);
         callSendAPIWithTemplate(sender_psid);
+        callSendAPIWithTemplate2(sender_psid);
       
     }
 
@@ -332,10 +333,178 @@ let callSendAPIWithTemplate = (sender_psid) => {
     });
 };
 
+let callSendAPIWithTemplate2 = (sender_psid) => {
+    // document fb message template
+    // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
+    let body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                  "elements": [
+                    {
+                      "buttons": [
+                        {
+                          "type": "web_url",
+                          "url": "https://siribox.sirivatana.co.th",
+                          "title": "ขอรายละเอียด"
+                        },
+                        {
+                          "type": "postback",
+                          "payload": "PriceList",
+                          "title": "สอบถามราคา"
+                        },
+                        {
+                          "payload": "Estimate",
+                          "type": "postback",
+                          "title": "คำนวนราคา"
+                        }
+                      ],
+                      "image_url": "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/306575973_613886746861410_4266199771047810890_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=Y-P4qXwTVTYAX-EheUj&_nc_ht=scontent.fbkk5-4.fna&oh=00_AT-kzV6dtAN8ND5Y1vCgjUkoldbL8l1puLmiEPKUHgXIzg&oe=6343B450",
+                      "title": "Packaging A",
+                      "subtitle": "Detail Packaging A",
+                      "default_action": {
+                        "webview_height_ratio": "full",
+                        "url": "https://siribox.sirivatana.co.th/",
+                        "type": "web_url"
+                      }
+                    },
+                    {
+                      "default_action": {
+                        "type": "web_url",
+                        "webview_height_ratio": "full",
+                        "url": "https://siribox.sirivatana.co.th/"
+                      },
+                      "image_url": "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/310428819_628543822062369_7249461621886316794_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=eB22wAN1riYAX_O-290&_nc_ht=scontent.fbkk5-3.fna&oh=00_AT8SkfqJRo5QhCn8HeBko8D61yU04EySJUDsOXhWGQDT8A&oe=63434B0B",
+                      "buttons": [
+                        {
+                          "url": "https://siribox.sirivatana.co.th",
+                          "title": "ขอรายละเอียด",
+                          "type": "web_url"
+                        },
+                        {
+                          "payload": "PriceList",
+                          "type": "postback",
+                          "title": "สอบถามราคา"
+                        },
+                        {
+                          "title": "คำนวนราคา",
+                          "type": "postback",
+                          "payload": "Estimate"
+                        }
+                      ],
+                      "title": "Packaging B",
+                      "subtitle": "Detail Packaging B"
+                    },
+                    {
+                      "default_action": {
+                        "type": "web_url",
+                        "url": "https://siribox.sirivatana.co.th/",
+                        "webview_height_ratio": "full"
+                      },
+                      "subtitle": "Detail Packaging C",
+                      "image_url": "https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/310669646_628542938729124_4845211762759805626_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=r1zaRESZR1QAX9R6lLK&_nc_ht=scontent.fbkk5-5.fna&oh=00_AT-P86C9iDHsf0zuHtZePnrxZrtTL1s2oVBgZumzrMh4tQ&oe=634389F0",
+                      "title": "Packaging C",
+                      "buttons": [
+                        {
+                          "title": "ขอรายละเอียด",
+                          "type": "web_url",
+                          "url": "https://siribox.sirivatana.co.th"
+                        },
+                        {
+                          "type": "postback",
+                          "title": "สอบถามราคา",
+                          "payload": "PriceList"
+                        },
+                        {
+                          "title": "คำนวนราคา",
+                          "payload": "Estimate",
+                          "type": "postback"
+                        }
+                      ]
+                    },
+                    {
+                      "image_url": "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/310437869_628542912062460_38473656999760349_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=ImSrYGRjUF8AX98IoRa&_nc_ht=scontent.fbkk5-3.fna&oh=00_AT9Fh6sZKxHA3Oh7tuMB7Dvx96eObVcLI3a-_e1cOR5-Xg&oe=63434884",
+                      "default_action": {
+                        "webview_height_ratio": "full",
+                        "type": "web_url",
+                        "url": "https://siribox.sirivatana.co.th/"
+                      },
+                      "subtitle": "Detail Packaging D",
+                      "buttons": [
+                        {
+                          "title": "ขอรายละเอียด",
+                          "type": "web_url",
+                          "url": "https://siribox.sirivatana.co.th"
+                        },
+                        {
+                          "title": "สอบถามราคา",
+                          "type": "postback",
+                          "payload": "PriceList"
+                        },
+                        {
+                          "title": "คำนวนราคา",
+                          "type": "postback",
+                          "payload": "Estimate"
+                        }
+                      ],
+                      "title": "Packaging D"
+                    },
+                    {
+                      "buttons": [
+                        {
+                          "title": "ขอรายละเอียด",
+                          "type": "web_url",
+                          "url": "https://siribox.sirivatana.co.th"
+                        },
+                        {
+                          "title": "สอบถามราคา",
+                          "type": "postback",
+                          "payload": "PriceList"
+                        },
+                        {
+                          "title": "คำนวนราคา",
+                          "type": "postback",
+                          "payload": "Estimate"
+                        }
+                      ],
+                      "default_action": {
+                        "url": "https://siribox.sirivatana.co.th/",
+                        "type": "web_url",
+                        "webview_height_ratio": "full"
+                      },
+                      "title": "Packaging E",
+                      "subtitle": "Detail Packaging E",
+                      "image_url": "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/310398255_628542892062462_5022278135923982550_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=kNEJs83HAS0AX-5vRpD&_nc_ht=scontent.fbkk5-4.fna&oh=00_AT9PqXFfCpCjrxE6J08f0q3At6fHFq2BdKyGk7uCLIrKhw&oe=634439E1"
+                    }
+                  ],
+                  "template_type": "generic"
+                }
+              }
+        }
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": body
+    }, (err, res, body) => {
+        if (!err) {
+             console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
+
 module.exports = {
   postWebhook: postWebhook,
   getWebhook: getWebhook
 };
 // git add .
-// git commit -m "first commit37"
+// git commit -m "first commit39"
 // refresh push
