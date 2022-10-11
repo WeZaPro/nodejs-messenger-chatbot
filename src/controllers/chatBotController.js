cdc63f8fbbcc93f5aa7722174fd68980cae07880
WeZaPro <taweesak9359@gmail.com>
2022-10-11 13:49:54 +0700

    first commit13

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -144,8 +144,8 @@ function handlePostback(sender_psid, received_postback) {
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
 
-    console.log("get response from callSendApi --> ",response);
-    
+    console.log("get response from callSendApi --> ",response.attachment.payload.elements);
+
     // Construct the message body
     let request_body = {
         "recipient": {
